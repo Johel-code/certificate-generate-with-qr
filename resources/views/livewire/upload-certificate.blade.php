cf06874f0903e9ed3d3293133c5cc734bbe674e3
Johel Saavedra <jbsch16@gmail.com>
2024-09-27 12:35:53 -0400

    Configuracion de texto en vista previa listo, (position, size, color)

--- resources/views/livewire/upload-certificate.blade.php
+++ resources/views/livewire/upload-certificate.blade.php
@@ -4,40 +4,74 @@
 
     @if ($image)
         <!-- Vista previa de la imagen subida -->
-        <div class="relative" x-data="{ textX: @entangle('textX'), textY: @entangle('textY'), textColor: @entangle('textColor'), textSize: @entangle('textSize') }">
+        <div class="relative" x-data="dragText()" style="width: 100%; height: auto;">
             <img src="{{ $image->temporaryUrl() }}" alt="Image" class="w-full h-auto">
 
-            <!-- Campo de texto editable -->
+            <!-- Campo de texto editable y arrastrable -->
             <div
                 x-bind:style="'left: ' + textX + 'px; top: ' + textY + 'px; position: absolute; color: ' + textColor + '; font-size: ' + textSize + 'px;'"
                 x-text="$wire.text"
-                @mousedown.prevent="$el.addEventListener('mousemove', moveText)"
-                @mouseup.prevent="$el.removeEventListener('mousemove', moveText)">
+                @mousedown="startDrag"
+                @mousemove="drag($event)"
+                @mouseup="stopDrag"
+                @mouseleave="stopDrag"
+                class="cursor-move select-none"
+                style="cursor: move;">
             </div>
         </div>
 
         <!-- Controles de personalización -->
-        <div class="mt-4">
+        <div class="mt-4" x-data="dragText()">
             <!-- Cambiar el texto -->
             <input type="text" wire:model="text" placeholder="Texto aquí">
 
             <!-- Cambiar el tamaño del texto -->
-            <input type="range" wire:model="textSize" min="10" max="100">
+            <input type="range" wire:model="textSize" min="10" max="100" x-model="textSize"> <!-- Añadido x-model para Alpine.js -->
 
             <!-- Cambiar el color del texto -->
-            <input type="color" wire:model="textColor">
+            <input type="color" wire:model="textColor" x-model="textColor"> <!-- Añadido x-model para Alpine.js -->
 
-            <!-- Cambiar la posición del texto -->
-            <label>X: <input type="number" wire:model="textX"></label>
-            <label>Y: <input type="number" wire:model="textY"></label>
+            <!-- Cambiar la posición del texto mediante inputs -->
+            <label>X: <input type="number" x-model="textX"></label> <!-- Usamos x-model para enlazar con Alpine.js -->
+            <label>Y: <input type="number" x-model="textY"></label> <!-- Usamos x-model para enlazar con Alpine.js -->
+        </div>
+
+        <div>
+            <label>Datos: {{ $textColor }}, {{ $textSize }}, {{ $textX }}, {{ $textY }}</label>
         </div>
     @endif
 
     <script>
-        function moveText(event) {
-            let textElement = event.target;
-            textElement.style.left = event.clientX + 'px';
-            textElement.style.top = event.clientY + 'px';
+        function dragText() {
+            return {
+                dragging: false,
+                textX: @entangle('textX'), // Sincronizar Alpine.js con Livewire
+                textY: @entangle('textY'),
+                textSize: @entangle('textSize') || 16, // Sincronizar tamaño del texto y establecer valor predeterminado
+                textColor: @entangle('textColor') || '#000000', // Sincronizar color del texto y establecer valor predeterminado
+                startX: 0,
+                startY: 0,
+
+                // Iniciar el arrastre
+                startDrag(event) {
+                    this.dragging = true;
+                    this.startX = event.clientX - this.textX;
+                    this.startY = event.clientY - this.textY;
+                },
+
+                // Mover el texto
+                drag(event) {
+                    if (this.dragging) {
+                        this.textX = event.clientX - this.startX;
+                        this.textY = event.clientY - this.startY;
+                    }
+                },
+
+                // Soltar el texto
+                stopDrag() {
+                    this.dragging = false;
+                }
+            }
         }
     </script>
 </div>
