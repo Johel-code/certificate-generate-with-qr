ea805658b50e6d395a11eef384707c506e5a682a
Johel Saavedra <jbsch16@gmail.com>
2024-10-10 20:35:09 -0400

    Upload of the CSV for configurations in preview

--- app/Livewire/UploadCertificate.php
+++ app/Livewire/UploadCertificate.php
@@ -4,9 +4,11 @@ namespace App\Livewire;
 
 use App\Models\Certificate;
 use Barryvdh\DomPDF\Facade\Pdf;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 use Livewire\WithFileUploads;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class UploadCertificate extends Component
 {
@@ -14,72 +16,141 @@ class UploadCertificate extends Component
     use WithFileUploads;
 
     public $image; // Para almacenar la imagen subida
-    public $text = "Texto de ejemplo"; // Texto inicial
-    public $textColor = "#000000"; // Color del texto
-    public $fontFamily = 'Arial';
-    public $textSize = 20; // Tamaño del texto
-    public $textX = 50; // Posición X del texto
-    public $textY = 50; // Posición Y del texto
+    public $csvData = []; // Almacenar el contenido del CSV
+    public $csvHeaders = []; // Almacenar los encabezados del CSV
+    public $selectedField; // Campo seleccionado para configurar
+    public $fieldsConfigurations = []; // Almacenar las configuraciones de cada campo
     public $csv;
+    public $textSize;
+    public $textColor;
+    public $fontFamily;
+    public $textX;
+    public $textY;
 
-    public function generateCertificate(){
+    public function updateFieldConfiguration($fieldId, array $data)
+    {
+        if (!empty($this->selectedField)) {
 
+            $this->fieldsConfigurations[$fieldId] = array_merge($this->fieldsConfigurations[$fieldId], $data);
+        }
+    }
+
+    public function updatedSelectedField()
+    {
+        // Actualiza las propiedades locales de Alpine.js
+        $this->textSize = $this->fieldsConfigurations[$this->selectedField]['textSize'] ?? null;
+        $this->textColor = $this->fieldsConfigurations[$this->selectedField]['textColor'] ?? null;
+        $this->fontFamily = $this->fieldsConfigurations[$this->selectedField]['fontFamily'] ?? null;
+        $this->textX = $this->fieldsConfigurations[$this->selectedField]['textX'] ?? null;
+        $this->textY = $this->fieldsConfigurations[$this->selectedField]['textY'] ?? null;
+        // ... y así sucesivamente para las demás propiedades
+    }
+
+    // Cargar y procesar el archivo CSV
+    public function updatedCsv()
+    {
         $this->validate([
-            'image' => 'required|image|max:1024',
+            'csv' => 'required|mimes:csv,txt|max:1024',
         ]);
 
-        // Procesar el archivo CSV para obtener la lista de nombres de usuarios
-        $csvPath = $this->csv->store('csv_files', 'public');
-        $csvFile = fopen(storage_path('app/public/' . $csvPath), 'r');
-        $users = [];
+        // Leer el archivo CSV
+        $path = $this->csv->getRealPath();
+        $data = array_map('str_getcsv', file($path));
 
-        while (($data = fgetcsv($csvFile, 1000, ',')) !== false) {
-            $userName = trim($data[0]); // Eliminar espacios en blanco antes y después del nombre
-            if (!empty($userName)) { // Solo agrega el nombre si no está vacío
-                $users[] = $userName;
-            }
+        // Guardar los encabezados (primera fila del CSV)
+        $this->csvHeaders = array_shift($data);
+
+        // Guardar el resto del contenido del CSV (los valores)
+        $this->csvData = $data;
+        foreach ($this->csvHeaders as $header) {
+            $this->fieldsConfigurations[$header] = [
+                'text' => $header,  // Mostrar el texto del header
+                'textSize' => 16,
+                'textColor' => '#000000',
+                'fontFamily' => 'Arial',
+                'textX' => 50,
+                'textY' => 50,
+            ];
         }
-
-        fclose($csvFile);
-
-        // Iterar sobre los nombres de los usuarios y generar un PDF para cada uno
-        foreach ($users as $user) {
-            $this->generateCertificateForUser($user);
-        }
-
-        session()->flash('message', 'Certificados generados con éxito.');
-
-        // return response()->streamDownload(function () use ($pdf) {
-        //     echo $pdf->stream();
-        // }, 'name.pdf');
+        // dd($this->fieldsConfigurations);
     }
 
-    public function generateCertificateForUser($userName)
-    {
-        $cleanUserName = trim($userName, "\xEF\xBB\xBF");
+    // public function updateFieldConfiguration($config)
+    // {
+    //     // Asegúrate de que el campo seleccionado tiene una configuración independiente
+    //     if ($this->selectedField) {
+    //         $this->fieldsConfigurations[$this->selectedField] = $config;
+    //         dd($config);
+    //     }
+    // }
 
-        $imagePath = $this->image->store('certificates', 'public');
+    // public function generateCertificate(){
 
-        $data = [
-            'image' => $imagePath,
-            'text' => $cleanUserName,
-            'textX' => $this->textX,
-            'textY' => $this->textY,
-            'textSize' => $this->textSize,
-            'textColor' => $this->textColor,
-            'fontFamily' => $this->fontFamily
-        ];
+    //     $this->validate([
+    //         'image' => 'required|image|max:1024',
+    //     ]);
 
-        $pdf = Pdf::loadView('pdf.certificate', $data)
-                    ->setPaper('A4', 'landscape')
-                    ->setOption('margin', '0');
+    //     // Procesar el archivo CSV para obtener la lista de nombres de usuarios
+    //     $csvPath = $this->csv->store('csv_files', 'public');
+    //     $csvFile = fopen(storage_path('app/public/' . $csvPath), 'r');
+    //     $users = [];
 
-        // Guardar cada PDF en una carpeta de certificados con el nombre del usuario
+    //     while (($data = fgetcsv($csvFile, 1000, ',')) !== false) {
+    //         $userName = trim($data[0]); // Eliminar espacios en blanco antes y después del nombre
+    //         if (!empty($userName)) { // Solo agrega el nombre si no está vacío
+    //             $users[] = $userName;
+    //         }
+    //     }
 
-        $pdfOutput = $pdf->output();
-        $fileName = 'certificates/' . preg_replace('/[^A-Za-z0-9\-]/', '', $cleanUserName) . '.pdf';
-        Storage::disk('public')->put($fileName, $pdfOutput);
-    }
+    //     fclose($csvFile);
+
+    //     // Iterar sobre los nombres de los usuarios y generar un PDF para cada uno
+    //     foreach ($users as $user) {
+    //         $this->generateCertificateForUser($user);
+    //     }
+
+    //     session()->flash('message', 'Certificados generados con éxito.');
+
+    //     // return response()->streamDownload(function () use ($pdf) {
+    //     //     echo $pdf->stream();
+    //     // }, 'name.pdf');
+    // }
+
+    // public function generateCertificateForUser($userName)
+    // {
+    //     $cleanUserName = trim($userName, "\xEF\xBB\xBF");
+
+    //     $imagePath = $this->image->store('certificates', 'public');
+
+    //     // Generar la URL del certificado
+    //     $certificateUrl = route('certificate.view', ['user' => $cleanUserName]);
+
+    //     // Generar el QR Code con la URL
+    //     $qrCode = base64_encode(QrCode::format('png')->size(300)->generate($certificateUrl));
+
+    //     $data = [
+    //         'image' => $imagePath,
+    //         'text' => $cleanUserName,
+    //         'textX' => $this->textX,
+    //         'textY' => $this->textY,
+    //         'textSize' => $this->textSize,
+    //         'textColor' => $this->textColor,
+    //         'fontFamily' => $this->fontFamily,
+    //         'qrCode' => $qrCode
+    //     ];
+
+    //     $pdf = Pdf::loadView('pdf.certificate', $data)
+    //                 ->setPaper('A4', 'landscape')
+    //                 ->setOption('margin', '0');
+
+    //     // Guardar cada PDF en una carpeta de certificados con el nombre del usuario
+
+    //     $pdfOutput = $pdf->output();
+    //     $fileName = 'certificates/' . preg_replace('/[^A-Za-z0-9\-]/', '', $cleanUserName) . '.pdf';
+    //     Storage::disk('public')->put($fileName, $pdfOutput);
+
+
+    // }
 
     public function render()
     {
--- resources/views/livewire/upload-certificate.blade.php
+++ resources/views/livewire/upload-certificate.blade.php
@@ -1,119 +1,131 @@
-<div class="flex">
-    <!-- Controles a la izquierda -->
-    <div class="w-1/3 p-4 bg-gray-100" x-data="dragText()" x-init="fontFamily = 'Arial'">
+<div class="">
+    <div x-data="dragText()">
+        <form wire:submit.prevent='generateCertificate' class="flex justify-center">
+            <div class="w-1/4 p-4">
+                <label class="block text-sm font-medium text-gray-700">Subir la imagen del certificado</label>
+                <input type="file" wire:model="image" class="block w-full mt-2 mb-4 text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-600 hover:file:bg-indigo-100">
 
-        <form wire:submit='generateCertificate'>
-
-            <label class="block text-sm font-medium text-gray-700">Subir la imagen del certificado</label>
-            <input type="file" wire:model="image" class="block w-full mt-2 mb-4 text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-600 hover:file:bg-indigo-100">
-
-            <label class="block text-sm font-medium text-gray-700">Subir archivo CSV con nombres de usuarios</label>
-            <input type="file" wire:model="csv" class="block w-full mt-2 mb-4 text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-600 hover:file:bg-indigo-100">
-
-            <!-- Cambiar el texto -->
-            <label class="block text-sm font-medium text-gray-700">Texto del certificado</label>
-            <input type="text" wire:model="text" placeholder="Texto aquí" class="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-
-            <!-- Cambiar el tamaño del texto -->
-            <label class="block mt-4 text-sm font-medium text-gray-700">Tamaño del texto</label>
-            <input type="range" wire:model="textSize" min="10" max="100" class="block w-full mt-1">
-
-            <!-- Cambiar el color del texto -->
-            <label class="block mt-4 text-sm font-medium text-gray-700">Color del texto</label>
-            <input type="color" wire:model="textColor" class="block w-full h-10 p-0 mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
-
-            <!-- Cambiar la fuente del texto -->
-            <label class="block mt-4 text-sm font-medium text-gray-700">Fuente del texto</label>
-            <select wire:model="fontFamily" class="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                <option value="Arial">Arial</option>
-                <option value="Times-Roman">Times-Roman</option>
-                <option value="Courier">Courier</option>
-            </select>
-
-            <!-- Cambiar la posición del texto mediante inputs -->
-            <div class="mt-4">
-                <label class="block text-sm font-medium text-gray-700">Posición X</label>
-                <input type="number" wire:model='textX' class="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-            </div>
-            <div class="mt-4">
-                <label class="block text-sm font-medium text-gray-700">Posición Y</label>
-                <input type="number" wire:model="textY" class="block w-full mt-1 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                <label class="block text-sm font-medium text-gray-700">Subir archivo CSV con nombres de usuarios</label>
+                <input type="file" wire:model="csv" class="block w-full mt-2 mb-4 text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-600 hover:file:bg-indigo-100">
             </div>
 
-            <button type="submit" class="block px-4 py-2 mt-4 font-semibold text-white bg-blue-600 rounded hover:bg-blue-700">
-                Generar Certificado
-            </button>
+            <div class="flex w-3/4">
+                @if ($selectedField)
+                    <div class="p-4">
+                        <label class="block mt-4 text-sm font-medium text-gray-700">Tamaño del texto</label>
+                        <input type="range" x-model="textSize" @input="$wire.updateFieldConfiguration('{{ $selectedField }}', { textSize })" min="10" max="100" class="block w-full mt-1">
+                    </div>
 
+                    <div class="p-4">
+                        <label class="block mt-4 text-sm font-medium text-gray-700">Color del texto</label>
+                        <input type="color" x-model="textColor" @input="$wire.updateFieldConfiguration('{{ $selectedField }}', { textColor })" class="block w-full h-10 p-0 mt-1 border border-gray-300 rounded-md">
+                    </div>
+
+                    <div class="p-4">
+                        <label class="block mt-4 text-sm font-medium text-gray-700">Fuente del texto</label>
+                        <select x-model="fontFamily" @input="$wire.updateFieldConfiguration('{{ $selectedField }}', { fontFamily })" class="block w-full mt-1 border border-gray-300 rounded-md">
+                            <option value="Arial">Arial</option>
+                            <option value="Times-Roman">Times-Roman</option>
+                            <option value="Courier">Courier</option>
+                        </select>
+                    </div>
+
+                    <div class="p-4">
+                        <label class="block mt-4 text-sm font-medium text-gray-700">Posición X</label>
+                        <input type="number" x-model="textX" @input="$wire.updateFieldConfiguration('{{ $selectedField }}', { textX })" class="block w-full mt-1 border border-gray-300 rounded-md">
+                    </div>
+
+                    <div class="p-4">
+                        <label class="block mt-4 text-sm font-medium text-gray-700">Posición Y</label>
+                        <input type="number" x-model="textY" @input="$wire.updateFieldConfiguration('{{ $selectedField }}', { textY })" class="block w-full mt-1 border border-gray-300 rounded-md">
+                    </div>
+
+                    <button type="submit" class="block p-4 font-semibold text-white bg-blue-600 rounded hover:bg-blue-700">Generar Certificado</button>
+                @endif
+            </div>
         </form>
 
-    </div>
-
-    <!-- Vista previa a la derecha -->
-    <div class="relative w-2/3 p-4">
-        @if ($image)
-            <!-- Vista previa de la imagen subida -->
-            <div class="relative" x-data="dragText()">
-                <img src="{{ $image->temporaryUrl() }}" alt="Vista previa del certificado" class="w-full h-auto border border-gray-300 rounded-md" id="previewImage">
-
-                <!-- Campo de texto editable y arrastrable -->
-                <div
-                    x-bind:style="'left: ' + textX + 'px; top: ' + textY + 'px; position: absolute; color: ' + textColor + '; font-size: ' + textSize + 'px; font-family: ' + fontFamily + ';'"
-                    x-text="$wire.text"
-                    @mousedown="startDrag"
-                    @mousemove="drag($event)"
-                    @mouseup="stopDrag"
-                    @mouseleave="stopDrag"
-                    class="font-bold cursor-move select-none"
-                    id="dragText"
-                    style="cursor: move;">
+        <div class="flex">
+            <div class="w-1/3 p-4 bg-gray-100">
+                <div class="w-1/2">
+                    <div class="mb-4">
+                        <label class="block text-sm font-medium text-gray-700">Selecciona un campo para configurar</label>
+                        <select wire:model.change="selectedField" class="block w-full mt-1 border border-gray-300 rounded-md">
+                            <option value="">Selecciona un campo</option>
+                            @foreach ($csvHeaders as $header)
+                                <option value="{{ $header }}">{{ $header }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
             </div>
-        @endif
+
+            <div class="relative w-2/3 p-4">
+                @if ($image)
+                    <div class="relative">
+                        <img src="{{ $image->temporaryUrl() }}" alt="Vista previa del certificado" class="w-full h-auto border border-gray-300 rounded-md" id="previewImage">
+
+                        <div x-data="dragText()">
+                            <template x-for="(fieldConfig, field) in fieldsConfigurations" :key="field">
+                                <div
+                                    @mousedown="startDrag(field, $event)"
+                                    @mouseup="stopDrag()"
+                                    @mousemove="dragField(field, $event)"
+                                    :style="`position: absolute; top: ${fieldConfig.textY}px; left: ${fieldConfig.textX}px; font-size: ${fieldConfig.textSize}px; color: ${fieldConfig.textColor}; font-family: ${fieldConfig.fontFamily};`"
+                                    class="draggable-field"
+                                    x-text="fieldConfig.text">
+                                </div>
+                            </template>
+                        </div>
+
+                    </div>
+                @endif
+            </div>
+        </div>
     </div>
 </div>
 
 <script>
     function dragText() {
         return {
-            dragging: false,
-            textX: @entangle('textX'),
-            textY: @entangle('textY'),
             textSize: @entangle('textSize'),
             textColor: @entangle('textColor'),
             fontFamily: @entangle('fontFamily'),
-            startX: 0,
-            startY: 0,
+            textX: @entangle('textX'),
+            textY: @entangle('textY'),
+            selectedField: null,
+            isDragging: false,
+            offsetX: 0,
+            offsetY: 0,
+            fieldsConfigurations: @entangle('fieldsConfigurations'),
 
-            // Iniciar el arrastre
-            startDrag(event) {
-                this.dragging = true;
-                this.startX = event.clientX - this.textX;
-                this.startY = event.clientY - this.textY;
+            startDrag(field, event) {
+                this.selectedField = field;
+                this.isDragging = true;
+                const fieldConfig = this.fieldsConfigurations[field];
+                this.offsetX = event.clientX - fieldConfig.textX;
+                this.offsetY = event.clientY - fieldConfig.textY;
             },
 
-            // Mover el texto
-            drag(event) {
-                if (this.dragging) {
-                    const previewImage = document.getElementById('previewImage');
-                    const previewRect = previewImage.getBoundingClientRect();
+            stopDrag() {
+                this.isDragging = false;
+            },
 
-                    const dragText = document.getElementById('dragText');
-                    const textRect = dragText.getBoundingClientRect();
-                    const textWidth = textRect.width; // Usar el ancho real del texto
-                    const textHeight = textRect.height; // Usar el alto real del texto
-
-                    // Limitar la posición X para que no se salga del área de vista previa
-                    this.textX = Math.max(0, Math.min(event.clientX - this.startX, previewRect.width - textWidth));
-
-                    // Limitar la posición Y para que no se salga del área de vista previa
-                    this.textY = Math.max(0, Math.min(event.clientY - this.startY, previewRect.height - textHeight));
+            dragField(field, event) {
+                if (this.isDragging && this.selectedField === field) {
+                    const fieldConfig = this.fieldsConfigurations[field];
+                    fieldConfig.textX = event.clientX - this.offsetX;
+                    fieldConfig.textY = event.clientY - this.offsetY;
                 }
             },
 
-            // Soltar el texto
-            stopDrag() {
-                this.dragging = false;
-            },
-        }
+            updateFieldConfiguration(fieldId, newConfig) {
+                if (fieldId === this.selectedField) {
+                    // Create a deep copy of the field configuration to prevent unintended updates
+                    const updatedConfig = { ...this.fieldsConfigurations[fieldId], ...newConfig };
+                    this.fieldsConfigurations[field] = updatedConfig;
+                }
+            }
+        };
     }
 </script>
